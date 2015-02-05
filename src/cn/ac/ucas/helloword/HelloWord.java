c2edb897e93e8216797c7ae9bfc2d4e6224a363d
zwl <zwl@ubuntu>
2015-02-05 16:15:36 +0800

    test again

--- src/cn/ac/ucas/helloword/HelloWord.java
+++ src/cn/ac/ucas/helloword/HelloWord.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cn.ac.ucas.helloword;
 
 /**
@@ -11,7 +10,9 @@ package cn.ac.ucas.helloword;
  * @author zwl
  */
 public class HelloWord {
+
     public static void main(String[] args) {
 	System.out.println("hello world");
+	System.out.println("hello again");
     }
 }
